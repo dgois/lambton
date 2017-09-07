85e31a4acf3fd5f882bc1963365b7a4c3caeaecc
Denis Gois <denisdwtg@gmail.com>
2017-09-07 19:34:27 -0400

    Review Data types, Functions and Constraints

--- CSD2204 - Second Class.sql
+++ CSD2204 - Second Class.sql
@@ -1,3 +1,5 @@
+-- DDL
+
 CREATE database mytestdb;
 
 USE mytestdb;
@@ -15,7 +17,8 @@ CREATE TABLE test_reserved_word
 	`use` VARCHAR(10);
 );
 
-SELECT * FROM student;
+-- show the complete form of create table
+SHOW CREATE TABLE products\G
 
 INSERT INTO student VALUES (
 1,
@@ -47,7 +50,23 @@ INSERT student VALUES (7, 'isaac', 'male', 75);
 INSERT student VALUES (8, 'sara', 'female', 40);
 INSERT student VALUES (9, 'matheus', 'male', 15);
 
+-- use of ALTER and clauses
+ALTER TABLE student ADD marks int(2);
+ALTER TABLE student ADD city varchar(50) AFTER gender;
+
+-- UPDATE
+UPDATE student SET city='Toronto', marks=100 WHERE sid IN (1,5,9);
+UPDATE student SET city='Delhi', marks=80 WHERE sid = 3;
+UPDATE student SET city='Mississauga', marks=100 WHERE snm='isaac' AND gender='male';
+UPDATE student SET city='Montreal', marks=55 WHERE gender='female' AND city IS NULL;
+UPDATE student SET marks=75 where marks is null;
+
+
+-- DML
+
 -- filtering
+SELECT * FROM student;
+
 SELECT * FROM student where sid = 1;
 
 SELECT * FROM student where sid >= 3;
@@ -85,11 +104,76 @@ SELECT * FROM student WHERE snm LIKE '_b%' or snm LIKE '_a%';
 SELECT * FROM student WHERE age BETWEEN 20 AND 30;
 SELECT * FROM student WHERE age IN(20,30,27);
 
--- use of ALTER and clauses
-ALTER TABLE student ADD marks int(2);
-ALTER TABLE student ADD city varchar(50) AFTER gender;
-
 -- use of AS
 select sid as StudentId FROM student;
 select sid StudentId FROM student;
 select sid as `Student Id` FROM student;
+
+-- Useful Functions
+SELECT VERSION();
+SELECT USER();
+SELECT CURDATE();
+SELECT CURRENT_DATE();
+SELECT NOW();
+SELECT SYSDATE();
+SELECT CURTIME();
+SELECT CURRENT_TIME();
+SELECT CURRENT_TIMESTAMP();
+SELECT CONCAT(snm, ' living in ', city) FROM student;
+SELECT FORMAT(1233344.33342, 2, 'br');
+
+CREATE TABLE Manufacturers (
+	Code INTEGER(3),
+	Name VARCHAR(50),
+	PRIMARY KEY (Code)
+);
+
+-- or
+
+CREATE TABLE Manufacturers (
+	Code integer(3) PRIMARY KEY,
+	Name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Products (
+	Code integer(3) PRIMARY KEY,
+	Name VARCHAR(50) NOT NULL,
+	Price decimal(4,2) NOT NULL,
+	Manufacturer integer(3),
+	FOREIGN KEY (Manufacturer)
+	REFERENCES Manufacturers(Code)
+);
+
+-- or
+
+CREATE TABLE Products (
+	Code integer(3) PRIMARY KEY,
+	Name VARCHAR(50) NOT NULL,
+	Price decimal(4,2) NOT NULL,
+	Manufacturer integer(3));
+	
+ALTER TABLE Products 
+ADD CONSTRAINT products_ibfk_1_denis 
+FOREIGN KEY (Manufacturer) 
+REFERENCES Manufacturers(Code);
+
+INSERT INTO Manufacturers VALUES (1, 'Nike');
+INSERT INTO Products VALUES (1, 'Nike Run Sneaker', 10.00, 1);
+
+INSERT INTO Manufacturers(Code,Name) VALUES(1,'Sony');
+INSERT INTO Manufacturers(Code,Name) VALUES(2,'Creative Labs');
+INSERT INTO Manufacturers(Code,Name) VALUES(3,'Hewlett-Packard');
+INSERT INTO Manufacturers(Code,Name) VALUES(4,'Iomega');
+INSERT INTO Manufacturers(Code,Name) VALUES(5,'Fujitsu');
+INSERT INTO Manufacturers(Code,Name) VALUES(6,'Winchester');
+
+INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(1,'Hard drive',240,5);
+INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(2,'Memory',120,6);
+INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(3,'ZIP drive',150,4);
+INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(4,'Floppy disk',5,6);
+INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(5,'Monitor',240,1);
+INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(6,'DVD drive',180,2);
+INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(7,'CD drive',90,2);
+INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(8,'Printer',270,3);
+INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(9,'Toner cartridge',66,3);
+INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(10,'DVD burner',180,2);

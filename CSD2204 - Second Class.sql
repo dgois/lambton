f1ccb78fc16432ae63ffe00e43ab001fd1e2a665
Denis Gois <denisdwtg@gmail.com>
2017-09-06 21:03:13 -0400

    Presenting comparison keywords

--- CSD2204 - Second Class.sql
+++ CSD2204 - Second Class.sql
@@ -47,6 +47,7 @@ INSERT student VALUES (7, 'isaac', 'male', 75);
 INSERT student VALUES (8, 'sara', 'female', 40);
 INSERT student VALUES (9, 'matheus', 'male', 15);
 
+-- filtering
 SELECT * FROM student where sid = 1;
 
 SELECT * FROM student where sid >= 3;
@@ -61,6 +62,34 @@ SELECT * FROM student LIMIT 3;
 
 SELECT * FROM student LIMIT 5,9;
 
+-- show the structure of a object
 DESC student;
 
+-- data matching using like
+-- end
+SELECT * FROM student WHERE snm LIKE 'ab%';
 
+-- begin
+SELECT * FROM student WHERE snm LIKE '%ab';
+
+-- using coringa char
+SELECT * FROM student WHERE snm LIKE '_b%';
+
+-- pay attetion for all clauses, in this example the second condition is false
+SELECT * FROM student WHERE snm LIKE '_b%' or '_a%';
+
+-- now it is true
+SELECT * FROM student WHERE snm LIKE '_b%' or snm LIKE '_a%';
+
+-- using between, in and any
+SELECT * FROM student WHERE age BETWEEN 20 AND 30;
+SELECT * FROM student WHERE age IN(20,30,27);
+
+-- use of ALTER and clauses
+ALTER TABLE student ADD marks int(2);
+ALTER TABLE student ADD city varchar(50) AFTER gender;
+
+-- use of AS
+select sid as StudentId FROM student;
+select sid StudentId FROM student;
+select sid as `Student Id` FROM student;

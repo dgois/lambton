e1bceb69e69c9275e197be1b0a46226537d6b004
Denis Gois <denisdwtg@gmail.com>
2017-09-13 21:04:35 -0400

    Constraint exercises

--- CSD2204.sql
+++ CSD2204.sql
@@ -199,4 +199,36 @@ SELECT name FROM products UNION SELECT code FROM manufacturers;
 SELECT name FROM manufacturers WHERE code IN (SELECT manufacturer FROM products WHERE price > 200);
 SELECT name FROM manufacturers WHERE code IN (SELECT manufacturer FROM products WHERE name LIKE '%drive%');
 
+-- Constraint exercises
+CREATE TABLE providers (code VARCHAR(3) PRIMARY KEY, name varchar(100));
 
+CREATE TABLE pieces (code INT PRIMARY KEY, name varchar(100));
+
+CREATE TABLE provides (
+	code INT AUTO_INCREMENT,
+	piece INT ,
+	provider VARCHAR(3),
+	price DECIMAL(10,2) CHECK(price>10),
+	PRIMARY KEY (code, piece, provider),
+	FOREIGN KEY (piece) REFERENCES pieces(code),
+	FOREIGN KEY (provider) REFERENCES providers(code)
+);
+
+INSERT INTO Providers(Code, Name) VALUES('HAL','Clarke Enterprises');
+INSERT INTO Providers(Code, Name) VALUES('RBT','Susan Calvin Corp.');
+INSERT INTO Providers(Code, Name) VALUES('TNBC','Skellington Supplies');
+
+INSERT INTO Pieces(Code, Name) VALUES(1,'Sprocket');
+INSERT INTO Pieces(Code, Name) VALUES(2,'Screw');
+INSERT INTO Pieces(Code, Name) VALUES(3,'Nut');
+INSERT INTO Pieces(Code, Name) VALUES(4,'Bolt');
+
+INSERT INTO Provides(Piece, Provider, Price) VALUES(1,'HAL',10);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(1,'RBT',15);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(2,'HAL',20);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(2,'RBT',15);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(2,'TNBC',14);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(3,'RBT',50);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(3,'TNBC',45);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(4,'HAL',5);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(4,'RBT',7);

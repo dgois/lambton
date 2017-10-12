7d0fd81adbbea995dd42c29528bd15a7462af61f
MacStudent <macstudent@20X-iMacXX-M-6.local>
2017-10-11 22:03:24 -0400

    Example app from apple documentation

---
+++ mad3115-ios-program-fundamentals/FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by MacStudent on 2017-10-11.
+//  Copyright © 2017 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field's user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+}
+

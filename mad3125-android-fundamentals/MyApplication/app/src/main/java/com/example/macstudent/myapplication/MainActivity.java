585508e447a16da624c40ab9363aec748d9d924d
MacStudent <macstudent@20X-iMacXX-M-6.local>
2017-12-04 22:02:36 -0500

    Add tests

---
+++ mad3125-android-fundamentals/MyApplication/app/src/main/java/com/example/macstudent/myapplication/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.macstudent.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class MainActivity extends AppCompatActivity implements Validator.ValidationListener {
+
+    //https://github.com/ragunathjawahar/android-saripaar
+    @NotEmpty
+    @Email
+    @InjectView(R.id.edtEmail)
+    EditText edtEmail;
+    @InjectView(R.id.btnValidate)
+    Button btnValidate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        final Validator validator = new Validator(this);
+        validator.setValidationListener(this);
+
+        btnValidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validator.validate();
+            }
+        });
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        Toast.makeText(this, "Yay! we got it right!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}

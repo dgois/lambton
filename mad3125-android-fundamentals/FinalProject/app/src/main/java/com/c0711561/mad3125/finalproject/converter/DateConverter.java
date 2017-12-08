cdc9ebd5ecb27bb6a740932a2722ad89362c7903
MacStudent <macstudent@20X-iMacXX-M-6.local>
2017-12-07 21:59:33 -0500

    Continue from here

--- mad3125-android-fundamentals/FinalProject/app/src/main/java/com/c0711561/mad3125/finalproject/converter/DateConverter.java
+++ mad3125-android-fundamentals/FinalProject/app/src/main/java/com/c0711561/mad3125/finalproject/converter/DateConverter.java
@@ -5,4 +5,6 @@ package com.c0711561.mad3125.finalproject.converter;
  */
 
 public class DateConverter {
+
+    //https://github.com/googlesamples/android-architecture-components/blob/1532c562bb4d25f8469471478195160fd96b5db7/BasicSample/app/src/main/java/com/example/android/persistence/db/AppDatabase.java
 }
